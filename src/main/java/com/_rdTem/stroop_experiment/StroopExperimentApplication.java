b4d7630555da4529b00cf9165b56e959934dca3e
Preta3418 <jason123qwe@gmail.com>
2025-12-01 10:57:48 +0900

    Initial commit

---
+++ src/main/java/com/_rdTem/stroop_experiment/StroopExperimentApplication.java
@@ -0,0 +1,13 @@
+package com._rdTem.stroop_experiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StroopExperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StroopExperimentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_rdTem/stroop_experiment/StroopExperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package com._rdTem.stroop_experiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StroopExperimentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
